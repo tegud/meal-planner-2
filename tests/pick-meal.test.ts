37cd1e1e4e3e4856050da131d242eed4043ff3c8
Steve Elliott <steve@tegud.net>
2021-01-03 07:25:09 +0000

    pick meal function

--- tests/pick-meal.test.ts
+++ tests/pick-meal.test.ts
@@ -1,8 +1,12 @@
 import { pickMeal } from '../lib/pick-meal';
 import { Meal } from '../lib/entities/meal';
+import { MealAllocationForDay } from '../lib/entities/meal-allocation';
 
 describe('pickMeal', () => {
+  let randomNumber: number;
+
   beforeEach(() => {
+    randomNumber = 0;
     jest.spyOn(global.Math, 'random').mockReturnValue(0.52);
   });
 
@@ -22,7 +26,17 @@ describe('pickMeal', () => {
   it('returns random meal option if more than one available and no historic meals', () => {
     const burgers = new Meal({ name: 'Burgers' });
     const hotdogs = new Meal({ name: 'Hotdogs' });
+    randomNumber = 1;
 
     expect(pickMeal([], [burgers, hotdogs])).toEqual(hotdogs);
   });
+  
+  // it('returns random meal option if more than one available and no historic meals', () => {
+  //   const burgers = new Meal({ name: 'Burgers' });
+  //   const hotdogs = new Meal({ name: 'Hotdogs' });
+
+  //   const lastAllocatedMeal = new MealAllocationForDay({ meal: hotdogs })
+
+  //   expect(pickMeal([], [burgers, hotdogs])).toEqual(burgers);
+  // });
 });
